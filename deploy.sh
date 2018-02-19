10b7d0d1bd129e76769f6645eccd5ba23f893b3d
codelingua <36545949+codelingua@users.noreply.github.com>
2018-02-19 09:23:59 -0500

    change npm install command

--- deploy.sh
+++ deploy.sh
@@ -113,18 +113,10 @@ fi
 # 2. Select node version
 selectNodeVersion
 
-# 2.5 Install NPM packages (dev)
-if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --only=dev
-  exitWithMessageOnError "npm failed"
-  cd - > /dev/null
-fi
-
 # 3. Install NPM packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
