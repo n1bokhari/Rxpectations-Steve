a55ff8c877ebaf252a6c7a38a17b8ada0a9b330b
codelingua <36545949+codelingua@users.noreply.github.com>
2018-02-19 14:44:04 -0500

    change license key

--- newrelic.js
+++ newrelic.js
@@ -11,8 +11,9 @@ exports.config = {
   app_name: ['dev-18f'],
   /**
    * Your New Relic license key.
+     license_key: '73a21f3cda43b2b79dc76d97f1c409fa151c8942',
    */
-  license_key: '73a21f3cda43b2b79dc76d97f1c409fa151c8942',
+  license_key: '8a8e8af54c5714d270e0b632a4aa1d15c659dc76',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
